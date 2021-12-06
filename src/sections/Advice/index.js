7c72b3aad0d68922654df34ece73138d9c7cbf3c
Steven Rico <steven.rico.dev@gmail.com>
2021-12-06 14:17:22 +0000

    Done.

--- src/sections/Advice/index.js
+++ src/sections/Advice/index.js
@@ -1,48 +1,11 @@
 import { useEffect, useState } from "react"
 
 function AdviceSection() {
-  const [slip, setSlip] = useState(null)
-  const [favouriteSlips, setFavouriteSlips] = useState([])
-
-  console.log({ slip })
-
-  function handleMoreAdviceClick() {
-    fetch("https://api.adviceslip.com/advice")
-      .then(res => res.json())
-      .then(res => setSlip(res.slip))
-  }
-
-  function handleSaveToFavouritesClick(slipToSave) {
-    setFavouriteSlips([...favouriteSlips, slipToSave])
-  }
-
-  useEffect(() => {
-    fetch("https://api.adviceslip.com/advice")
-      .then(res => res.json())
-      .then(res => setSlip(res.slip))
-  }, [])
-
-  if (!slip) return <p>Loading...</p>
-
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip">
-        <h3>Some Advice</h3>
-        <p>{slip.advice}</p>
-        <button onClick={handleMoreAdviceClick}>Get More Advice</button>
-        <button onClick={() => handleSaveToFavouritesClick(slip)}>
-          Save to Favourties
-        </button>
-      </section>
-      <section className="favourtite-slips-list">
-        <h3>Favourite Advice Slips</h3>
-        <ul>
-          {favouriteSlips.map((slip, index) => (
-            <li key={index}>{slip.advice}</li>
-          ))}
-        </ul>
-      </section>
+      <section className="adivce-slip"></section>
+      <section className="favourtite-slips-list"></section>
     </section>
   )
 }
--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,48 +1,10 @@
 import { useEffect, useState } from "react"
 
 function ArtsSection() {
-  const [art, setArt] = useState(null)
-
-  console.log({ art })
-
-  useEffect(() => {
-    fetch("https://api.artic.edu/api/v1/artworks")
-      .then(res => res.json())
-      .then(res => setArt(res.data))
-  }, [])
-
-  if (!art) return <p>Loading...</p>
-
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container">
-        <ul className="art-list">
-          {art.map((artwork, index) => {
-            const { title, artist_title, image_id, subject_titles } =
-              artwork
-
-            const imageUrl = `https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`
-            return (
-              <li key={index}>
-                {image_id && (
-                  <div className="frame">
-                    <img src={imageUrl} />
-                  </div>
-                )}
-                <h3>{title}</h3>
-                <p>Artist: {artist_title}</p>
-                <h4>Artistic Subjects:</h4>
-                <ul>
-                  {subject_titles.map((subject, index) => (
-                    <li key={index}>{subject}</li>
-                  ))}
-                </ul>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+      <div className="scroll-container"></div>
     </section>
   )
 }
