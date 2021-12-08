cc3bb64971c0030daac4fb37d190761f9ef2f25d
DWAUUD BELKO <belkodawuud@gmail.com>
2021-12-08 11:38:35 +0100

    changes
    Co-authored-by: bilaal-s <bilaal-s@users.noreply.github.com>

--- src/sections/Users/index.js
+++ src/sections/Users/index.js
@@ -1,12 +1,34 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
 function UsersSection() {
+  const [users, setUsers] = useState([]);
+  //console.log(Users)
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data.results);
+        setUsers(data.results);
+      });
+  }, []);
+  console.log(users);
+  /*users.map((user) => {
+          return <p>{user.name}</p>;
+        })*/
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        {users.map((user) => (
+          <li>
+            {user.name.first}
+            {user.name.last}
+            {user.name.title}
+          </li>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
-
-export default UsersSection
+//console.log(results);
+export default UsersSection;
